2499a39b4cf4c92f066f4e11848c124eccc7f8f5
fsahat1 <fsahat1@etf.unsa.ba>
2021-10-18 08:40:46 +0200

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,9 @@
+package ba.unsa.etf.rpr;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x = Math.round((int)(Math.random()*10)); //nasumican broj od 1 do  10
+        System.out.println("vrijednost "+x); // da li ce biti isti pri svakom poketanju?
+    }
+}
